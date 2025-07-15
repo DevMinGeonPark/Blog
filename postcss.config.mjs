169c89b13d73dfa43261f4a673db171ece3ef01b
DevminGeonPark <devmin7985@gmail.com>
2025-07-15 14:54:20 +0900

    fix: Add postcss-stylus to postcss.config.mjs for Stylus file processing

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,11 +1,13 @@
 import postcssImport from 'postcss-import';
 import postcssNesting from 'tailwindcss/nesting/index.js';
 import tailwindcss from 'tailwindcss';
+import postcssStylus from 'postcss-stylus';
 
 export default {
     plugins: {
         'postcss-import': postcssImport,          // to combine multiple css files
         'tailwindcss/nesting': postcssNesting,
         tailwindcss: tailwindcss,
+        'postcss-stylus': postcssStylus,
     }
 };
